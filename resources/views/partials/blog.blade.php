cdd757c8719031198f6ee8e7cda2464de6aa79e4
zeppyx13 <gungnands@gmail.com>
2024-03-16 03:01:10 +0800

    login page

--- resources/views/partials/blog.blade.php
+++ resources/views/partials/blog.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark navbar-expand-lg bg-transparant fs-4">
     <div class="container-fluid">
-        <a class="navbar-brand fs-1" href="/">Gu-Blog</a>
+        <a class="navbar-brand fs-2" href="/">Gu-Blog</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -20,8 +20,8 @@
                     <a class="nav-link" href="/login">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ $status === 'Login' ? 'active' : '' }}  btn btn-danger rounded ms-3 p-2 fs-4"
-                        href="/about"><i class="bi bi-door-open"></i><strong> Login</strong></a>
+                    <a class="nav-link btn btn-outline-secondary rounded ms-3 p-2 fs-5" href="/Login"><i
+                            class="bi bi-door-open"></i><strong> Login</strong></a>
                 </li>
             </ul>
         </div>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark navbar-expand-lg bg-transparant fs-4">
     <div class="container-fluid">
-        <a class="navbar-brand fs-1" href="/">Gu-Blog</a>
+        <a class="navbar-brand fs-2" href="/">Gu-Blog</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -20,7 +20,7 @@
                     <a class="nav-link {{ $status3 }}" href="/about">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link btn btn-danger rounded ms-3 p-2 fs-4" href="/login"><i
+                    <a class="nav-link btn btn-outline-secondary rounded ms-3 p-2 fs-5" href="/Login"><i
                             class="bi bi-door-open"></i><strong> Login</strong></a>
                 </li>
             </ul>
